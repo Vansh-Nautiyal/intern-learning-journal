2321af87e117ccf7017dfd740a9a393ab78573be
Vansh-Nautiyal <vanshnautiyal187@gmail.com>
2026-06-05 13:36:47 +0530

    Fixed Division by zero error

--- docs/script.js
+++ docs/script.js
@@ -4,25 +4,31 @@ const addBtn = document.getElementById("addBtn");
 const subBtn = document.getElementById("subBtn");
 const mulBtn = document.getElementById("mulBtn");
 const divBtn = document.getElementById("divBtn");
-addBtn.addEventListener("click",function(){
+addBtn.addEventListener("click", function () {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    resultArea.innerText = `${num1} + ${num2} = ${num1+num2}`;
+    resultArea.innerText = `${num1} + ${num2} = ${num1 + num2}`;
 });
 
-mulBtn.addEventListener("click",function(){
+mulBtn.addEventListener("click", function () {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    resultArea.innerText = `${num1} * ${num2} = ${num1*num2}`;
+    resultArea.innerText = `${num1} * ${num2} = ${num1 * num2}`;
 });
 
-subBtn.addEventListener("click",function(){
+subBtn.addEventListener("click", function () {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    resultArea.innerText = `${num1} - ${num2} = ${num1-num2}`;
+    resultArea.innerText = `${num1} - ${num2} = ${num1 - num2}`;
 });
-divBtn.addEventListener("click",function(){
+
+divBtn.addEventListener("click", function () {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    resultArea.innerText = `${num1} / ${num2} = ${num1/num2}`;
+    if (num2 != 0) {
+        resultArea.innerText = `${num1} / ${num2} = ${num1 / num2}`;
+    }
+    else{
+        resultArea.innerText = `Error !! Division by Zero !!`;
+    }
 });
