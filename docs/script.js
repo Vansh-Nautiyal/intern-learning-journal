9a943b3f1be41de4e3c458c9788255a6c6a76dd2
Vansh-Nautiyal <vanshnautiyal187@gmail.com>
2026-06-05 12:09:21 +0530

    Calculator App

---
+++ docs/script.js
@@ -0,0 +1,28 @@
+console.log(num1, num2)
+const resultArea = document.getElementById("results");
+const addBtn = document.getElementById("addBtn");
+const subBtn = document.getElementById("subBtn");
+const mulBtn = document.getElementById("mulBtn");
+const divBtn = document.getElementById("divBtn");
+addBtn.addEventListener("click",function(){
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    resultArea.innerText = `${num1} + ${num2} = ${num1+num2}`;
+});
+
+mulBtn.addEventListener("click",function(){
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    resultArea.innerText = `${num1} * ${num2} = ${num1*num2}`;
+});
+
+subBtn.addEventListener("click",function(){
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    resultArea.innerText = `${num1} - ${num2} = ${num1-num2}`;
+});
+divBtn.addEventListener("click",function(){
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    resultArea.innerText = `${num1} / ${num2} = ${num1/num2}`;
+});
