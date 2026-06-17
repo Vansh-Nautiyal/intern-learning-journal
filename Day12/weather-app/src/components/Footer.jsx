54a8853aa1ed4a5ea4756ec6c4c09b69ee467009
Vansh-Nautiyal <vanshnautiyal187@gmail.com>
2026-06-17 12:17:06 +0530

    Weather app

---
+++ Day12/weather-app/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return (
+    <footer className="footer">
+        <p>@Weather App | Built using React and WeatherAPI.com</p>
+    </footer>
+    );
+}
+export default Footer;
---
+++ Day12/weather-app/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar(){
+    return (
+        <nav className="navbar">
+            <h2>Weather App</h2>
+            <p>Search for weather using City names</p>
+        </nav>
+    );
+}
+
+export default Navbar;
