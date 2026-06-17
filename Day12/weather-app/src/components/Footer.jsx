d4252eaad328cb045010210428aa4da74627b23b
Vansh-Nautiyal <vanshnautiyal187@gmail.com>
2026-06-17 12:20:53 +0530

    Removed Day12

--- Day12/weather-app/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer(){
-    return (
-    <footer className="footer">
-        <p>@Weather App | Built using React and WeatherAPI.com</p>
-    </footer>
-    );
-}
-export default Footer;
