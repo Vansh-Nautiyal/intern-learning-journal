14a09976653582db9a5e1bb519cd64eb7e1e2148
Vansh-Nautiyal <vanshnautiyal187@gmail.com>
2026-07-11 12:09:44 +0530

    final changes

--- Day21/blog-app/backend/server.js
+++ Day21/blog-app/backend/server.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      process.env.FRONTEND_URL,
+      "https://blog-post-five-mu.vercel.app/",
     ],
     credentials: true,
   })
